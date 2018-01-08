010840b55827193ee589eee1d6203ea946509d83
Sandeep Krishnan <sandeep.krishnan@hotmail.com>
2018-01-08 18:03:38 +0530

    Conditionals

---
+++ conditions/for-loop.pl
@@ -0,0 +1,17 @@
+my @array = (1..10);
+
+for(my $i = 0; $i < 10; $i++) {
+	print "$array[$i] \n";
+}
+
+foreach my $value (@array) {
+	print "The value is $value \n";
+}
+
+%hash = (1 => 'one', 2 => 'two', 3 => 'three');
+
+print "===================\nPrinting values of map hash \n ";
+
+foreach my $value (values %hash) {
+	print "value is $value \n";
+}
---
+++ conditions/if-else.pl
@@ -0,0 +1,8 @@
+$a = 10;
+
+if($a == 10) {
+	print "a is 10 \n";
+} 
+else {
+	print "a is not 10 \n";
+}
