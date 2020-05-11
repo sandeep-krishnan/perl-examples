6a560decf88d017534aa6194eb436e328ad03e14
Sandeep Krishnan <krsandeep@athenahealth.com>
2020-05-11 16:45:57 +0530

    File::Spec

--- modules/modules.pl
+++ modules/modules.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings FATAL => 'all';
 #selective import of methods
 use File::Basename qw(dirname basename);
+use File::Spec;
 
 #can't rename this method to dirname as long as import is done
 # one way is to skip the import explicitly qw / /
@@ -13,3 +14,8 @@ sub dirname1() {
 my $basename = basename("/Users/krsandeep/code");
 my $dirname = dirname("/Users/krsandeep/code");
 print("Base : $basename.. Dir : $dirname");
+
+
+#File::Spec
+my $filespec = File::Spec->catfile('/Users/krsandeep', 'code','sand/Perl/modules', 'modules.pl');
+print("\nFile spec ", $filespec);
